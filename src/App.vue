1313cb0e1ccee5fc05b6ab4c2e33b8e549df60c6
MatteoZattini <matteozattini@hotmail.com>
2024-06-03 15:50:39 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
+import store from './data/store.js'
+
+export default {
+    name: "App",
+
+    components: {
+      AppHeader,
+        AppMain,
+        AppFooter,
+    },
+
+    data() {
+        return {
+          store,
+        }
+    },
+
+    methods: {
+      
+    },
+
+    mounted() {
+
+    }
+};
+
+</script>
+
+<template>
+<AppHeader />
+<AppMain />
+</template>
+
+<style>
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,33 @@
+<script>
+import store from '../data/store.js'
+
+export default {
+    name: "AppHeader",
+
+    components: {
+
+    },
+
+    data() {
+        return {
+            store,
+        }
+    },
+
+    methods: {
+      
+    },
+
+    mounted() {
+
+    }
+};
+
+</script>
+
+<template>
+
+</template>
+
+<style>
+</style>
